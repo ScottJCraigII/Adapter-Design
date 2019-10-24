ac00257d3b16e7527b231986bc408661c5959f00
240031 <54606544+240031@users.noreply.github.com>
2019-10-24 12:06:49 -0400

    Add files via upload
    Update file type

---
+++ Knight.java
@@ -0,0 +1,10 @@
+public class Knight implements Warrior {
+
+     public void attack()
+     {
+      System.out.println("Attacking:Im bringing you down!")
+     }
+     public void defend()
+     {
+     System.out.println("Defending:We must protect the castle")
+     }
